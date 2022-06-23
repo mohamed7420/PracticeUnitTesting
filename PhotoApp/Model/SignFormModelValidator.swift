d9b2a3472eae3f39a6a884348add73851093298d
Mohamed Osama <mohamed.osama9812@gmail.com>
2022-06-23 19:11:18 +0200

    Adding unit testing for validate firstName

---
+++ PhotoApp/Model/SignFormModelValidator.swift
@@ -0,0 +1,8 @@
+//
+//  SignFormModelValidator.swift
+//  PhotoApp
+//
+//  Created by Mohamed osama on 23/06/2022.
+//
+
+import Foundation
--- PhotoAppTests/SignupFormModelValidatorTests.swift
+++ PhotoAppTests/SignupFormModelValidatorTests.swift
@@ -1,14 +1,13 @@
 //
-//  PhotoAppTests.swift
+//  SignupFormModelValidatorTests.swift
 //  PhotoAppTests
 //
 //  Created by Mohamed osama on 23/06/2022.
 //
 
 import XCTest
-@testable import PhotoApp
 
-class PhotoAppTests: XCTestCase {
+class SignupFormModelValidatorTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
