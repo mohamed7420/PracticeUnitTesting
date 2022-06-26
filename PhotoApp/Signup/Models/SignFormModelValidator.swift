2b4b496a56ee29061ebd48c7766b2761a7025d6c
Mohamed Osama <mohamed.osama9812@gmail.com>
2022-06-26 20:34:13 +0200

    refactor folder structure

--- PhotoApp/Signup/Models/SignFormModelValidator.swift
+++ PhotoApp/Signup/Models/SignFormModelValidator.swift
@@ -7,12 +7,7 @@
 
 import Foundation
 
-protocol SignupFormModelValidatorProtocol{
-    func validateFirstName(firstName: String) -> Bool
-    func validateLastName(lastName: String) -> Bool
-    func validateEmail(email: String) -> Bool
-    func validatePasswordMatching(password: String , re_password: String) -> Bool
-}
+
 
 class SignupFormModelValidator: SignupFormModelValidatorProtocol{
 
